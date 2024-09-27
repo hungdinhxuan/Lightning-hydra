d2fd24b85d7b1f171079c123957c3eb2045c9f8b
Hung Dinh Xuan <hungdinhxdev@gmail.com>
2024-09-28 01:20:52 +0900

    Define wavlm+conformertcm, xlsr+conformertcm, wavlm+vib, xlsr+vib

---
+++ src/models/components/xlsr_conformertcm.py
@@ -0,0 +1,206 @@
+import random
+from typing import Union
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+from torch.nn.modules.transformer import _get_clones
+import fairseq
+try:
+    from model.loss_metrics import supcon_loss
+    from model.RawNet3.model import RawNet3
+    from model.RawNet3.RawNetBasicBlock import Bottle2neck
+    from model.conformer_tcm.model import MyConformer
+except:
+    from .loss_metrics import supcon_loss
+    from .RawNet3.model import RawNet3
+    from .RawNet3.RawNetBasicBlock import Bottle2neck
+    from .conformer_tcm.model import MyConformer
+
+
+############################
+## FOR fine-tuned SSL MODEL
+############################
+
+
+class SSLModel(nn.Module):
+    def __init__(self):
+        super(SSLModel, self).__init__()
+        
+        cp_path = '/data/hungdx/asvspoof5/model/pretrained/xlsr2_300m.pt'   # Change the pre-trained XLSR model path. 
+        model, cfg, task = fairseq.checkpoint_utils.load_model_ensemble_and_task([cp_path])
+        self.model = model[0]
+        
+        self.out_dim = 1024
+        return
+
+    def extract_feat(self, input_data, is_train=True):
+        
+        # put the model to GPU if it not there
+        # if next(self.model.parameters()).device != input_data.device \
+        #    or next(self.model.parameters()).dtype != input_data.dtype:
+        #     self.model.to(input_data.device, dtype=input_data.dtype)
+        #     self.model.train()
+
+        # input should be in shape (batch, length)
+        if input_data.ndim == 3:
+            input_tmp = input_data[:, :, 0]
+        else:
+            input_tmp = input_data
+            
+        # [batch, length, dim]
+        emb = self.model(input_tmp, mask=False, features_only=True)['x']
+        # print(emb.shape)
+        return emb
+    
+    def forward(self, x):
+        # x is a tensor of [batch, length]
+        return self.extract_feat(x)
+
+
+class Model(nn.Module):
+    def __init__(self, args, device, is_train = True):
+        super().__init__()
+        self.device = device
+        self.is_train = is_train
+        self.flag_fix_ssl = args['flag_fix_ssl']
+        self.contra_mode = args['contra_mode']
+        self.loss_type = args['loss_type']
+        
+        self.loss_CE = nn.CrossEntropyLoss(weight = torch.FloatTensor([float(1-args['ce_loss_weight']), float(args['ce_loss_weight'])]).to(device))
+
+        ####
+        # create network wav2vec 2.0
+        ####
+        self.front_end = SSLModel()
+        self.LL = nn.Linear(self.front_end.out_dim, args['conformer']['emb_size'])
+        self.first_bn = nn.BatchNorm2d(num_features=1)
+        self.drop = nn.Dropout(0.5, inplace=True)
+        self.selu = nn.SELU(inplace=True)
+        
+        self.backend=MyConformer(**args['conformer'])
+        self.loss_CE = nn.CrossEntropyLoss()
+        
+        self.sim_metric_seq = lambda mat1, mat2: torch.bmm(
+            mat1.permute(1, 0, 2), mat2.permute(1, 2, 0)).mean(0)
+
+    def _forward(self, x):
+        x.requires_grad = True
+        #-----------------RawNet3-----------------#
+        x = self.front_end(x) #(bs,frame_number,frontend_out_dim)
+        x = self.LL(x) #(bs,frame_number,feat_out_dim)
+        
+        feats = x
+        x = x.unsqueeze(dim=1) # add channel #(bs, 1, frame_number, 256)
+        x = self.first_bn(x)
+        x = self.selu(x)
+        x = x.squeeze(dim=1)
+
+        # output [batch, 2]
+        # emb [batch, emb_size]
+        output, emb = self.backend(x)
+        output = F.log_softmax(output, dim=1)
+        if (self.is_train):
+            return output, feats, emb
+        return output
+    
+    def forward(self, x_big):
+        # make labels to be a tensor of [bz]
+        # labels = labels.squeeze(0)
+        if (x_big.dim() == 3):
+            x_big = x_big.transpose(0,1)
+            batch, length, sample_per_batch = x_big.shape
+            # x_big is a tensor of [length, batch, sample per batch]
+            # transform to [length, batch*sample per batch] by concat last dim
+            x_big = x_big.transpose(1,2)
+            x_big = x_big.reshape(batch * sample_per_batch, length)
+        if (self.is_train):
+            # x_big is a tensor of [1, length, bz]
+            # convert to [bz, length]
+            # x_big = x_big.squeeze(0).transpose(0,1)
+            output, feats, emb = self._forward(x_big)
+            # calculate the loss
+            return output, feats, emb
+        else:
+            # in inference mode, we don't need the emb
+            # the x_big now is a tensor of [bz, length]
+            return self._forward(x_big)
+        
+    
+    def loss(self, output, feats, emb, labels, config, info=None):
+        real_bzs = output.shape[0]
+        # print("real_bzs", real_bzs)
+        # print("labels", labels)
+        L_CE = 1/real_bzs *self.loss_CE(output, labels)
+        
+        # reshape the feats to match the supcon loss format
+        feats = feats.unsqueeze(1)
+        # print("feats.shape", feats.shape)
+        L_CF1 = 1/real_bzs *supcon_loss(feats, labels=labels, contra_mode=self.contra_mode, sim_metric=self.sim_metric_seq)
+        # reshape the emb to match the supcon loss format
+        emb = emb.unsqueeze(1)
+        emb = emb.unsqueeze(-1)
+        # print("emb.shape", emb.shape)
+        L_CF2 = 1/real_bzs *supcon_loss(emb, labels=labels, contra_mode=self.contra_mode, sim_metric=self.sim_metric_seq)
+        if self.loss_type == 1:
+            return {'L_CE':L_CE, 'L_CF1':L_CF1, 'L_CF2':L_CF2}
+        elif self.loss_type == 2:
+            return {'L_CE':L_CE, 'L_CF1':L_CF1}
+        elif self.loss_type == 3:
+            return {'L_CE':L_CE, 'L_CF2':L_CF2}
+        # ablation study
+        elif self.loss_type == 4:
+            return {'L_CE':L_CE}
+        elif self.loss_type == 5:
+            return {'L_CF1':L_CF1, 'L_CF2':L_CF2}
+
+# if __name__ == '__main__':
+#     import yaml
+#     # LoRA
+#     from peft import LoraConfig, TaskType, PeftModel, get_peft_model
+
+
+#     config = yaml.load(open("/data/hungdx/asvspoof5/configs_21/xlsr_aasist_baseline.yaml", 'r'), Loader=yaml.FullLoader)
+#     device = "cpu"
+#     model = Model(config['model'], device)
+#     # Ensure model is moved to the correct device
+#     model = model.to(device)
+
+#     # print("Hello")
+#     # print(model)
+
+#     # Trying apply lora
+#     # Using the best config from this paper
+#     # https://arxiv.org/pdf/2306.05617
+#     target_modules = ["q_proj", "v_proj"]
+#     r = 4
+
+#     lora_config = LoraConfig(
+#         r=r,
+#         target_modules=target_modules,
+#         #modules_to_save=config['lora']['modules_to_save'], # Modules to continue fine-tuning
+#     )
+#     peft_model = get_peft_model(model, lora_config)
+#     #print(peft_model)
+#     peft_model.print_trainable_parameters()
+
+#     x = torch.randn(1, 64000)
+#     x_ori = x.detach().clone()
+    
+#     x = x.to(device)
+
+#     output, feats, emb = peft_model(x)
+
+#     cross_entropy = nn.CrossEntropyLoss()
+#     labels = torch.tensor([1])
+#     labels = labels.to(device)
+
+#     loss = cross_entropy(output, labels)
+
+#     loss.backward()
+
+#     # Ensure that after backward pass, the x_ori and x still have the same value
+#     assert torch.allclose(x_ori, x.detach().clone()), "The input x has been changed after backward pass"
+#     print("Done")
---
+++ src/models/components/xlsr_vib.py
@@ -0,0 +1,224 @@
+import random
+from typing import Union
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+import fairseq
+import os
+import torch.nn.functional as F
+
+class SSLModel(nn.Module):
+    def __init__(self):
+        super(SSLModel, self).__init__()
+        
+        cp_path = '/data/hungdx/asvspoof5/model/pretrained/xlsr2_300m.pt'   # Change the pre-trained XLSR model path. 
+        model, cfg, task = fairseq.checkpoint_utils.load_model_ensemble_and_task([cp_path])
+        self.model = model[0]
+        
+        self.out_dim = 1024
+        return
+
+    def extract_feat(self, input_data):
+        
+        # put the model to GPU if it not there
+        # if next(self.model.parameters()).device != input_data.device \
+        #    or next(self.model.parameters()).dtype != input_data.dtype:
+        #     self.model.to(input_data.device, dtype=input_data.dtype)
+        #     self.model.train()
+
+        
+        if True:
+            # input should be in shape (batch, length)
+            if input_data.ndim == 3:
+                input_tmp = input_data[:, :, 0]
+            else:
+                input_tmp = input_data
+                
+            # [batch, length, dim]
+            emb = self.model(input_tmp, mask=False, features_only=True)['x']
+        return emb
+
+class DropoutForMC(nn.Module):
+    """Dropout layer for Bayesian model
+    THe difference is that we do dropout even in eval stage
+    """
+
+    def __init__(self, p, dropout_flag=True):
+        super(DropoutForMC, self).__init__()
+        self.p = p
+        self.flag = dropout_flag
+        return
+
+    def forward(self, x):
+        return torch.nn.functional.dropout(x, self.p, training=self.flag)
+
+
+class BackEnd(nn.Module):
+    """Back End Wrapper
+    """
+
+    def __init__(self, input_dim, out_dim, num_classes,
+                 dropout_rate, dropout_flag=True):
+        super(BackEnd, self).__init__()
+
+        # input feature dimension
+        self.in_dim = input_dim
+        # output embedding dimension
+        self.out_dim = out_dim
+        # number of output classes
+        self.num_class = num_classes
+
+        # dropout rate
+        self.m_mcdp_rate = dropout_rate
+        self.m_mcdp_flag = dropout_flag
+
+        # a simple full-connected network for frame-level feature processing
+        self.m_frame_level = nn.Sequential(
+            nn.Linear(self.in_dim, self.in_dim),
+            nn.GELU(),
+            torch.nn.Dropout(self.m_mcdp_rate),
+            # DropoutForMC(self.m_mcdp_rate,self.m_mcdp_flag),
+
+            nn.Linear(self.in_dim, self.in_dim),
+            nn.GELU(),
+            torch.nn.Dropout(self.m_mcdp_rate),
+            # DropoutForMC(self.m_mcdp_rate,self.m_mcdp_flag),
+
+            nn.Linear(self.in_dim, self.out_dim),
+            nn.GELU(),
+            torch.nn.Dropout(self.m_mcdp_rate)
+        )
+        # DropoutForMC(self.m_mcdp_rate,self.m_mcdp_flag))
+
+        # linear layer to produce output logits
+        self.m_utt_level = nn.Linear(self.out_dim, self.num_class)
+
+        return
+
+    def forward(self, feat):
+        """ logits, emb_vec = back_end_emb(feat)
+
+        input:
+        ------
+          feat: tensor, (batch, frame_num, feat_feat_dim)
+
+        output:
+        -------
+          logits: tensor, (batch, num_output_class)
+          emb_vec: tensor, (batch, emb_dim)
+
+        """
+        # through the frame-level network
+        # (batch, frame_num, self.out_dim)
+        feat_ = self.m_frame_level(feat)
+
+        # average pooling -> (batch, self.out_dim)
+        feat_utt = feat_.mean(1)
+
+        # output linear
+        logits = self.m_utt_level(feat_utt)
+        return logits
+
+
+class VIB(nn.Module):
+    def __init__(self, input_dim, hidden_dim, latent_dim, m_mcdp_rate=0.5, mcdp_flag=True):
+        super(VIB, self).__init__()
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.latent_dim = latent_dim
+        self.m_mcdp_rate = m_mcdp_rate
+        self.m_mcdp_flag = mcdp_flag
+
+        # Encoder
+        self.encoder = nn.Sequential(
+            nn.Linear(self.input_dim, self.hidden_dim),
+            nn.GELU(),
+            torch.nn.Dropout(self.m_mcdp_rate),
+            nn.Linear(self.hidden_dim, self.hidden_dim),
+            nn.GELU(),
+            torch.nn.Dropout(self.m_mcdp_rate),
+        )
+
+        # Latent space
+        self.fc_mu = nn.Linear(self.hidden_dim, latent_dim)
+        self.fc_var = nn.Linear(self.hidden_dim, latent_dim)
+
+        # Decoder
+        self.decoder = nn.Sequential(
+            nn.Linear(latent_dim, self.hidden_dim),
+            nn.GELU(),
+            torch.nn.Dropout(self.m_mcdp_rate),
+            nn.Linear(self.hidden_dim, self.input_dim),
+        )
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5 * logvar)
+        eps = torch.randn_like(std)
+        return mu + eps * std
+
+    def forward(self, x):
+        encoded = self.encoder(x)
+        mu, logvar = self.fc_mu(encoded), self.fc_var(encoded)
+        z = self.reparameterize(mu, logvar)
+        decoded = self.decoder(z)
+        return z, decoded, mu, logvar
+
+
+class Model(nn.Module):
+    def __init__(self, args, device, is_train = False):
+        super().__init__()
+        self.device = device
+        self.is_train = is_train
+        self.flag_fix_ssl = args['flag_fix_ssl']
+        self.contra_mode = args['contra_mode']
+        self.loss_type = args['loss_type']
+        ####
+        # create network wav2vec 2.0
+        ####
+        self.ssl_model = SSLModel()
+        self.LL = nn.Linear(self.ssl_model.out_dim, 128)
+        self.first_bn = nn.BatchNorm2d(num_features=1)
+        self.first_bn1 = nn.BatchNorm2d(num_features=64)
+        self.drop = nn.Dropout(0.5, inplace=True)
+        self.selu = nn.SELU(inplace=True)
+
+        self.loss_CE = nn.CrossEntropyLoss()
+        self.VIB = VIB(128, 128, 64)
+        self.backend = BackEnd(64, 64, 2, 0.5, False)
+        self.gelu = nn.GELU()
+
+        self.sim_metric_seq = lambda mat1, mat2: torch.bmm(
+            mat1.permute(1, 0, 2), mat2.permute(1, 2, 0)).mean(0)
+        # Post-processing
+
+    def _forward(self, x):
+        # -------pre-trained Wav2vec model fine tunning ------------------------##
+        # if self.flag_fix_ssl:
+        #     with torch.no_grad():
+        #         x_ssl_feat = self.ssl_model.extract_feat(
+        #             x.squeeze(-1), is_train=False)
+        # else:
+        #     x_ssl_feat = self.ssl_model.extract_feat(
+        #         x.squeeze(-1), is_train=self.is_train)  # (bs,frame_number,feat_dim)
+        x_ssl_feat = self.ssl_model.extract_feat(x.squeeze(-1))
+
+        x = self.LL(x_ssl_feat)  # (bs,frame_number,feat_out_dim)
+        feats = x
+        x = self.gelu(x)
+
+        # VIB
+        # x [batch, frame_number, 64]
+        x, decoded, mu, logvar = self.VIB(x)
+
+        # output [batch, 2]
+        # emb [batch, 128]
+        output = self.backend(x)
+
+        return output
+
+    def forward(self, x_big):
+        return self._forward(x_big)
+
